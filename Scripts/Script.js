ff82d7d290600c14c9274d86f2b20ef3782641ea
saikrishna5907 <saikrishna5907@gmail.com>
2018-06-06 01:59:07 +1000

    1st day practice

---
+++ Scripts/Script.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="angular.min.js" />
+var myApp = angular.module("myModule", [])
+
+myApp.controller("myController", function ($scope) {
+    var employee = [
+            { firstName: "sai", lastName: "krishna", gender : "Male" },
+            { firstName: "krishna", lastName: "tej", gender : "Male" },
+            { firstName: "rajesh", lastName: "dev", gender  : "Male" },
+            { firstName: "ravi", lastName: "samanchi", gender :  "Male"},
+             ];
+    $scope.employee = employee;
+});
