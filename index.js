39b3226b17ed5c25508fc14c9219430314b36d25
saikrishna5907 <saikrishna5907@gmail.com>
2018-06-06 19:31:48 +1000

    deploying

---
+++ index.js
@@ -0,0 +1,10 @@
+﻿var express = require("express");
+
+var app = express();
+var port = process.env.PORT || 8080;
+
+app.get("/", function (req, res) {
+    res.send("welcome to node js app on heroku!");
+});
+
+app.listen(port);
