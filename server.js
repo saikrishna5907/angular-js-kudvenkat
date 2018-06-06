9bbfca77f4c22c9a97d5de3a295776c1cb333341
saikrishna5907 <saikrishna5907@gmail.com>
2018-06-06 18:06:13 +1000

    updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app=express();
 //set port number
-var port = process.env.port || 8080
+var port = process.env.PORT || 8080
 
 app.use(express.static(__dirname + "/public"));
 
