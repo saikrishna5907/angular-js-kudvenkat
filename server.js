16627f7cc7167a24e0f7ddd7c4331d358bad9a6b
saikrishna5907 <saikrishna5907@gmail.com>
2018-06-06 18:37:39 +1000

    testing

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app=express();
 var port = process.env.PORT || 8080
 
 app.use(express.static(__dirname + "/public"));
-
+app.set('view engine','ejs');
 // set up routes
 app.get("/",function(req,res){
 	res.render("htmlpage");
